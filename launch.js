e8bbc9e25adb5456dfeb9e62130e85314746439f
tarcisiocorte <tarcisiocorte@gmail.com>
2024-05-24 23:03:07 -0300

    add the lauch to use debug

---
+++ launch.js
@@ -0,0 +1,13 @@
+{
+    "version": "0.2.0",
+    "configurations": [
+      {
+        "type": "node",
+        "request": "launch",
+        "name": "Launch Program",
+        "skipFiles": ["<node_internals>/**"],
+        "program": "${workspaceFolder}/index.js"  // Adjust the path if your file is in a subfolder
+      }
+    ]
+  }
+  
