20525eca472c71c67cf82ba3f60076b6ca8007d7
tarcisiocorte <tarcisiocorte@gmail.com>
2024-05-25 19:55:51 -0300

    add twoNumberSum algorithm

---
+++ twoNumberSum.js
@@ -0,0 +1,17 @@
+function twoNumberSum(array, targetSum) {
+    const nums = {};
+    for (const num of array) {
+        const potentialMatch = targetSum - num;
+        if (potentialMatch in nums) {
+            return [potentialMatch, num];
+        } else {
+            nums[num] = true;
+        }
+    }
+    return [];
+}
+
+const array = [3, 5, -4, 8, 11, 1, -1, 6];
+const targetSum = 10;
+const result = twoNumberSum(array, targetSum);
+console.log(result);
