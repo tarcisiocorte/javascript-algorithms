3e5abdd1ee9b600f0af8174f7be99bb8b9832745
tarcisiocorte <tarcisiocorte@gmail.com>
2024-05-25 19:56:37 -0300

    add small performance test

---
+++ performanceTestToCompareMath.js
@@ -0,0 +1,9 @@
+const right = 10;
+const left = 5;
+const width = right - left; // Simple subtraction
+
+console.time('Math.abs');
+for (let i = 0; i < 1000000; i++) {
+    let width = Math.abs(right - left);
+}
+console.timeEnd('Math.abs');
