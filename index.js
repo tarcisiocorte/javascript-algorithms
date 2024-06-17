b28b3a6a3505c60141164075f99824d24e331b21
tarcisiocorte <tarcisiocorte@gmail.com>
2024-06-17 15:51:52 -0300

    use palindrome permutation solution 3

--- index.js
+++ index.js
@@ -1,12 +1,70 @@
+const PalindromePermutation = require('./challenge/pallindrome-permutation');
+
+// Exemplo de uso
+const input = "Tact Coa";
+  
+//const result1 = PalindromePermutation.isPermutationOfPalindromeSolution1(input);
+//console.log(`Solução 1: "${input}" é uma permutação de um palíndromo? ${result1}`);
+
+//const result2 = PalindromePermutation.isPermutationOfPalindromeSolution2(input);
+//console.log(`Solução 2: "${input}" é uma permutação de um palíndromo? ${result2}`);
+
+const result3 = PalindromePermutation.isPermutationOfPalindromeSolution3(input);
+console.log(`Solução 3: "${input}" é uma permutação de um palíndromo? ${result3}`);
+
+
+
+/*** *
+const URLify = require('./challenge/remove-white-space-1.3.js');
+// let test URLify
+//const str = 'Mr John Smith    ';
+// give another example
+const str = 'Copa SP de futebol junior 2021                ';
+const trueLength = 30;
+
+
+const url = URLify(str, trueLength);
+console.log(url); // Expected output: 'Mr%20John%20Smith'
+
+**/
+
+/***
 const LinkedList = require("./linked-lists/linkedList");
 
-const list = new LinkedList();
-list.add(1);
-list.add(2);
-list.add(3);
-list.print(); // 1, 2, 3
-list.remove(2);
-list.print(); // 1, 3
+const list1 = new LinkedList();
+const list2 = new LinkedList();
+list1.add(2);
+list1.add(4);
+list1.add(3);
+
+list2.add(5);
+list2.add(6);
+list2.add(4);
+
+var addTwoNumbers = function(l1, l2) {
+    let currentL1 = l1.head;
+    let currentL2 = l2.head;
+    let numL1 = "";
+    let numL2 = "";
+    while(currentL1 && currentL2){
+        numL1 += currentL1.value;
+        numL2 += currentL2.value;
+
+        currentL1 = currentL1.next;
+        currentL2 = currentL2.next;        
+    } 
+    return (Math.floor(numL1) + Math.floor(numL2)).toString().split('').reverse();  
+};
+
+console.log(addTwoNumbers(list1, list2));
+***/
+
+
+
+
+// addTwoNumbers([2,4,3], [5,6,4]); // Expected output: 807
+// addTwoNumbers([0], [0]); // Expected output: 0
+// addTwoNumbers([9,9,9,9,9,9,9], [9,9,9,9]); // Expected output: 10009998
 
 /*** 
 const firstMissingPositive = require('./cyclic-sort/firstMissingPositive');
