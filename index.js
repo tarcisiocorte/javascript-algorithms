f84beb18e9ca346da8be3ace8deb57b38c5b20cd
tarcisiocorte <tarcisiocorte@gmail.com>
2024-06-21 14:57:13 -0300

    add tests for string compression

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+/** 
+ * exectar testes na classe Compression
+const StringCompressor = require("./challenge/string-compression-1.6");
+const compressor = new StringCompressor();
+
+//console.log(compressor.compressBad("aabcccccaaa")); // Output: a2b1c5a3
+console.log(compressor.compressString("aabcccccaaa")); // Output: a2b1c5a3
+
+console.log(compressor.compressBad("abc")); // Output: abc
+console.log(compressor.compressString("abc")); // Output: abc
+
+console.log(compressor.compressBad("aabbcc")); // Output: aabbcc
+console.log(compressor.compressString("aabbcc")); // Output: aabbcc
+
+console.log(compressor.compressBad("aaAAaa")); // Output: a2A2a2
+console.log(compressor.compressString("aaAAaa")); // Output: a2A2a2
+***/
+
+/***
+ * 
+ *  
+const OneWay = require("./challenge/one-way-1.5");
+const oneway = new OneWay();
+const input1 = "pale";
+const input2 = "ple";
+const result = oneway.oneEditAwayOptimized(input1, input2);
+console.log(`"${input1}", "${input2}": ${result}`);
+
+**/
+
+/** 
 const PalindromePermutation = require('./challenge/pallindrome-permutation');
 
 // Exemplo de uso
@@ -11,5 +42,4 @@ const input = "Tact Coa";
 
 const result3 = PalindromePermutation.isPermutationOfPalindromeSolution3(input);
 console.log(`Solução 3: "${input}" é uma permutação de um palíndromo? ${result3}`);
-
-
+***/
