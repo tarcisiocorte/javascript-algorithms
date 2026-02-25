9fe873653bb64e771f665536ad506071b33a40ee
Tarcísio Corte <tarcisiocorte@gmail.com>
2026-02-25 03:04:11 -0300

    feat: organize test cases for LinkedList methods into describe blocks

--- linked-lists/linkedList.test.js
+++ linked-lists/linkedList.test.js
@@ -1,32 +1,59 @@
 const LinkedList = require('./linkedList');
 
 describe('LinkedList', () => {
-    test('add() should append nodes and increase size', () => {
-        const list = new LinkedList();
-        expect(list.size).toBe(0);
 
-        list.add(1);
-        expect(list.size).toBe(1);
-        expect(list.head.value).toBe(1);
+    describe('add()', () => {
+        test('add() should append nodes and increase size', () => {
+            const list = new LinkedList();
+            expect(list.size).toBe(0);
 
-        list.add(2);
-        expect(list.size).toBe(2);
-        expect(list.head.next.value).toBe(2);
+            list.add(1);
+            expect(list.size).toBe(1);
+            expect(list.head.value).toBe(1);
 
-        list.add(3);
-        expect(list.size).toBe(3);
-        expect(list.head.next.next.value).toBe(3);
+            list.add(2);
+            expect(list.size).toBe(2);
+            expect(list.head.next.value).toBe(2);
+
+            list.add(3);
+            expect(list.size).toBe(3);
+            expect(list.head.next.next.value).toBe(3);
+        });
     });
 
-    test('addFirst() should prepend a node to the beginning', () => {
-        const list = new LinkedList();
-        list.addFirst(10);
-        expect(list.size).toBe(1);
-        expect(list.head.value).toBe(10);
+    describe('addFirst()', () => {
+        test('addFirst() should prepend a node to the beginning', () => {
+            const list = new LinkedList();
+            list.addFirst(10);
+            expect(list.size).toBe(1);
+            expect(list.head.value).toBe(10);
 
-        list.addFirst(20);
-        expect(list.size).toBe(2);
-        expect(list.head.value).toBe(20);
-        expect(list.head.next.value).toBe(10);
+            list.addFirst(20);
+            expect(list.size).toBe(2);
+            expect(list.head.value).toBe(20);
+            expect(list.head.next.value).toBe(10);
+        });
+    });
+
+    describe('get()', () => {
+        test('should throw an error if index is out of bounds', () => {
+            const list = new LinkedList();
+            expect(() => list.get(0)).toThrow('Index out of bounds');
+           
+            list.add(1);
+            expect(() => list.get(1)).toThrow('Index out of bounds');
+
+            expect(() => list.get(-1)).toThrow('Index out of bounds');
+        });
+
+        test('should return a correct value for a positive index', () => {
+            const list = new LinkedList();
+            list.add(1);
+            list.add(2);
+            list.add(3);
+            expect(list.get(0)).toBe(1);
+            expect(list.get(1)).toBe(2);
+            expect(list.get(2)).toBe(3);
+        });
     });
 });
