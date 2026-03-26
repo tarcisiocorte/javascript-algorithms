2ef075f35ee5e12e62a5a4578f941efc3d8689f0
Tarcísio Corte <tarcisiocorte@gmail.com>
2026-03-26 16:29:36 -0300

    refactor: simplify searchInRotatedSortedArray function by improving readability and reducing code complexity

--- exercises/searchInRotateSortedArray/index.js
+++ exercises/searchInRotateSortedArray/index.js
@@ -1,33 +1,27 @@
 var searchInRotatedSortedArray = function (nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
+  let left = 0;
+  let right = nums.length - 1;
 
-    while (left <= right) {
-        const mid = Math.floor((left + right) / 2);
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const midValue = nums[mid];
 
-        if (nums[mid] === target) {
-            return mid;
-        }
+    if (midValue === target) return mid;
 
-        // Determine which side is sorted
-        if (nums[left] <= nums[mid]) {
-            // Left side is sorted
-            if (nums[left] <= target && target < nums[mid]) {
-                right = mid - 1; // Target is in the left half
-            } else {
-                left = mid + 1; // Target is in the right half
-            }
-        } else {
-            // Right side is sorted
-            if (nums[mid] < target && target <= nums[right]) {
-                left = mid + 1; // Target is in the right half
-            }
-            else {
-                right = mid - 1; // Target is in the left half
-            }
-        }
+    if (nums[left] <= midValue) {
+      if (nums[left] <= target && target < midValue) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else if (midValue < target && target <= nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
     }
-    return -1; // Target not found
+  }
+
+  return -1;
 };
 
 module.exports = searchInRotatedSortedArray;
